19783dda5000c6de4ac02504db65aa24eba0e737
Charlotte Westerberg <cw222kq@student.lnu.se>
2017-10-03 11:05:34 +0200

    Add a comment in the Console class

--- 1DV607UML/src/view/Console.java
+++ 1DV607UML/src/view/Console.java
@@ -24,6 +24,7 @@ public class Console {
 	boolean verboseList = true;
 	
 	public void listAllMembers(ArrayList<Member>list){
+		/*"Compact List" name, ID and number of boats*/
 		if(compactList){
 			for(int i=0; i<list.size();i++){
 				System.out.println(list.get(i).getName() + " " + list.get(i).getID() + " " + list.get(i).getNumberOfBoats());
